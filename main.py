d466655ca065865e9e75d824503d9d0062c62234
Danyal Akarca <da434@cam.ac.uk>
2024-08-29 16:18:29 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,56 @@
+class TicTacToe:
+    def __init__(self):
+        self.board = [' ' for _ in range(9)]
+        self.current_player = 'X'
+
+    def make_move(self, position):
+        if self.board[position] == ' ':
+            self.board[position] = self.current_player
+            self.current_player = 'O' if self.current_player == 'X' else 'X'
+            return True
+        return False
+
+    def check_winner(self):
+        winning_combinations = [
+            [0, 1, 2], [3, 4, 5], [6, 7, 8],  # Rows
+            [0, 3, 6], [1, 4, 7], [2, 5, 8],  # Columns
+            [0, 4, 8], [2, 4, 6]  # Diagonals
+        ]
+        for combo in winning_combinations:
+            if self.board[combo[0]] == self.board[combo[1]] == self.board[combo[2]] != ' ':
+                return self.board[combo[0]]
+        if ' ' not in self.board:
+            return 'Tie'
+        return None
+
+    def print_board(self):
+        for i in range(0, 9, 3):
+            print(' | '.join(self.board[i:i+3]))
+            if i < 6:
+                print('---------')
+
+def play_game():
+    game = TicTacToe()
+    winner = None
+    while not winner:
+        game.print_board()
+        try:
+            move = int(input(f"Player {game.current_player}, enter your move (0-8): "))
+            if 0 <= move <= 8:
+                if game.make_move(move):
+                    winner = game.check_winner()
+                else:
+                    print("That position is already occupied. Try again.")
+            else:
+                print("Invalid move. Please enter a number between 0 and 8.")
+        except ValueError:
+            print("Invalid input. Please enter a number.")
+    
+    game.print_board()
+    if winner == 'Tie':
+        print("It's a tie!")
+    else:
+        print(f"Player {winner} wins!")
+
+if __name__ == "__main__":
+    play_game()
---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="tictactoe-yourusername",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[],
+    entry_points={
+        'console_scripts': [
+            'tictactoe=tictactoe.main:play_game',
+        ],
+    },
+    author="Danyal Akarca",
+    author_email="danyal.akarca@gmail.com",
+    description="A simple Tic-Tac-Toe game",
+    long_description="This package provides a command-line implementation of the classic Tic-Tac-Toe game.",
+    long_description_content_type="text/markdown",
+    url="https://github.com/DanAkarca/tictactoe",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.10',
+)
